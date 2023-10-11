bf32850886ce7568377706192b6b107074d45ca1
mehdi <mehdiarghan@gmail.com.git/>
2023-10-11 18:39:20 +0330

    Calling createTableStudent method and createTableUniversity method into main class

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,7 +1,11 @@
 package view;
 
+import repository.Method;
+
 public class Main {
     public static void main(String[] args) {
-
+        Method method=new Method();
+        method.createTableStudent();
+        method.createTableUniversity();
     }
 }
